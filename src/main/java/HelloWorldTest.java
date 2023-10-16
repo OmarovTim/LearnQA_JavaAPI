4d3c327cb3550d184f65af71b51e60d2a2f49018
Timur Omarov <111358221+OmarovTim@users.noreply.github.com>
2023-10-16 22:26:56 +0500

    HelloWorldTest

--- src/main/java/HelloWorldTest.java
+++ src/main/java/HelloWorldTest.java
@@ -1,15 +1,27 @@
+import io.restassured.http.Headers;
+import org.junit.jupiter.api.Test;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
-import org.junit.jupiter.api.Test;
 
-public class HelloWorldTest {
+class testEx6Redirect {
 
     @Test
-    public void testHelloWorld(){
-        Response response = RestAssured
-                .get("https://playground.learnqa.ru/api/get_text")
+    public void testRestAssured() {
+        Response response;
+        response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
                 .andReturn();
-        response.prettyPrint();
+
+
+        int statusCode = response.getStatusCode();
+        System.out.println(statusCode);
+
+        String locationHeader = response.getHeader("Location");
+        System.out.println(locationHeader);
     }
 }
